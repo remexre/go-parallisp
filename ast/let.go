6a2f973576693525b86ef5b5b7bc7d0a4a74a95b
Nathan Ringo <remexre@gmail.com>
2016-07-02 05:08:31 -0500

    Adds multiple bindings to letrecs.

--- ast/let.go
+++ ast/let.go
@@ -8,14 +8,12 @@ type Binding struct {
 
 // Let represents a let-expression.
 type Let struct {
-	// TODO Multiple Bindings
-	Binding
-	Body Node
+	Bindings []Binding
+	Body     Node
 }
 
 // LetRec represents a letrec-expression.
 type LetRec struct {
-	// TODO Multiple Bindings
-	Binding
-	Body Node
+	Bindings []Binding
+	Body     Node
 }
